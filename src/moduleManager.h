8081f09b035a2d6f674f6d4b4b0689b285d7d873
Shan <147230569+shanji361@users.noreply.github.com>
2025-07-24 15:04:56 -0400

    added turn 90 degrees cmd

--- src/moduleManager.h
+++ src/moduleManager.h
@@ -400,6 +400,10 @@ void readSignal() {
   read_infrared();  //  newCmdIdx = 1
 #endif
   read_serial();  //  newCmdIdx = 2
+   if (strcmp(newCmd, "turn90R") == 0) {
+    turnRight90DegreesFirmware();
+    newCmdIdx = 0;  // reset command index to mark the command as handled
+  }
 #ifdef BT_BLE
   detectBle();  //  newCmdIdx = 3;
   readBle();
@@ -438,10 +442,7 @@ void readSignal() {
 #endif
 #ifdef GESTURE
     if (moduleActivatedQ[indexOfModule(EXTENSION_GESTURE)])
-    {
-      gestureGetValue = read_gesture();
-      // PTHL("gestureValue02:", gestureGetValue);
-    }
+      read_gesture();
 #endif
 #ifdef PIR
     if (moduleActivatedQ[indexOfModule(EXTENSION_PIR)])
